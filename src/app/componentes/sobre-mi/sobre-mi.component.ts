bf71683bc586cbd9a61b7620128a6b1822f51e59
juan <juan.cano549@pascualbravo.edu.co>
2025-04-29 18:46:15 -0500

    aceptar cambios

--- src/app/componentes/sobre-mi/sobre-mi.component.ts
+++ src/app/componentes/sobre-mi/sobre-mi.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sobre-mi',
-  standalone: true,
   templateUrl: './sobre-mi.component.html',
   styleUrl: './sobre-mi.component.css'
 })

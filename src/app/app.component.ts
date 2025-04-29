26728a8b4418f4b52ef1bcd20cea1da46ba21abc
juan <juan.cano549@pascualbravo.edu.co>
2025-04-29 10:50:47 -0500

    aceptar cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SobreMiComponent],  
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/componentes/sobre-mi/sobre-mi.component.ts
+++ src/app/componentes/sobre-mi/sobre-mi.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sobre-mi',
-  imports: [],
+  standalone: true,
   templateUrl: './sobre-mi.component.html',
   styleUrl: './sobre-mi.component.css'
 })

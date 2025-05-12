547859989a3e0942cae301b92f4e26659e21ff75
juan <juan.cano549@pascualbravo.edu.co>
2025-05-12 11:12:27 -0500

    aceptar cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
-import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SobreMiComponent, ContactoComponent],  
+  imports: [RouterOutlet],  
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
